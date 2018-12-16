dbc6f7e22d0f9df5051323fb149c2112fa4ecd31
Bojan Miljevic <miljevic.bojan@yahoo.com>
2018-12-16 16:09:31 +0100

    Final

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScript : MonoBehaviour
+{
+    public GameObject picture1;
+    public GameObject picture2;
+    public GameObject picture3;
+    public GameObject picture4;
+    public GameObject picture5;
+    public GameObject picture6;
+    public GameObject picture7;
+    public GameObject picture8;
+    public GameObject picture9;
+    public GameObject picture10;
+    public GameObject picture11;
+    public GameObject picture12;
+    public GameObject picture13;
+    public GameObject picture14;
+    public GameObject picture15;
+    public GameObject picture16;
+    public GameObject picture17;
+    public GameObject picture18;
+    public GameObject picture19;
+    public GameObject picture20;
+    public GameObject picture21;
+    public GameObject picture22;
+    public GameObject picture23;
+    public GameObject picture24;
+    public GameObject picture25;
+    public GameObject picture26;
+    public GameObject picture27;
+    public GameObject picture28;
+    public GameObject picture29;
+    public GameObject picture30;
+    public int i = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+                if (i == 0)
+                {
+                    picture1.SetActive(false);
+                }
+                i++;
+                if (i == 1)
+                {
+                    picture2.SetActive(false);
+                }
+                if (i == 2)
+                {
+                    picture3.SetActive(false);
+                }
+                if (i == 3)
+                {
+                    picture4.SetActive(false);
+                }
+                if (i == 4)
+                {
+                    picture5.SetActive(false);
+                }
+                if (i == 5)
+                {
+                    picture6.SetActive(false);
+                }
+                if (i == 6)
+                {
+                    picture7.SetActive(false);
+                }
+                if (i == 7)
+                {
+                    picture8.SetActive(false);
+                }
+                if (i == 8)
+                {
+                    picture9.SetActive(false);
+                }
+                if (i == 9)
+                {
+                    picture10.SetActive(false);
+                }
+                if (i == 10)
+                {
+                    picture11.SetActive(false);
+                }
+                if (i == 11)
+                {
+                    picture12.SetActive(false);
+                }
+                if (i == 12)
+                {
+                    picture13.SetActive(false);
+                }
+                if (i == 13)
+                {
+                    picture14.SetActive(false);
+                }
+                if (i == 14)
+                {
+                    picture15.SetActive(false);
+                }
+                if (i == 15)
+                {
+                    picture16.SetActive(false);
+                }
+                if (i == 16)
+                {
+                    picture17.SetActive(false);
+                }
+                if (i == 17)
+                {
+                    picture18.SetActive(false);
+                }
+                if (i == 18)
+                {
+                    picture19.SetActive(false);
+                }
+                if (i == 19)
+                {
+                    picture20.SetActive(false);
+                }
+                if (i == 20)
+                {
+                    picture21.SetActive(false);
+                }
+                if (i == 21)
+                {
+                    picture22.SetActive(false);
+                }
+                if (i == 22)
+                {
+                    picture23.SetActive(false);
+                }
+                if (i == 23)
+                {
+                    picture24.SetActive(false);
+                }
+                if (i == 24)
+                {
+                    picture25.SetActive(false);
+                }
+                if (i == 25)
+                {
+                    picture26.SetActive(false);
+                }
+                if (i == 26)
+                {
+                    picture27.SetActive(false);
+                }
+                if (i == 27)
+                {
+                    picture28.SetActive(false);
+                }
+                if (i == 28)
+                {
+                    picture29.SetActive(false);
+                }
+            }
+        }
+    }
+

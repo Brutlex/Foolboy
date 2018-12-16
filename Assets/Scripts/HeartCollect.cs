387b82d1823be2f34fb06736805c6d21c0c1cd30
edisyo <45883565+edisyo@users.noreply.github.com>
2018-12-16 14:40:13 +0100

    before hearts

---
+++ Assets/Scripts/HeartCollect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect : MonoBehaviour
+{
+
+    public GameObject hearts;
+    public GameObject explosion;
+    public Animator ExplosionAnimator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+}

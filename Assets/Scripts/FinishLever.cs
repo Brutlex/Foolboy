115547130acbe226e07bcb8d2a50b98341dbcb0f
edisyo <45883565+edisyo@users.noreply.github.com>
2018-12-16 13:37:15 +0100

    Finish lever

---
+++ Assets/Scripts/FinishLever.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLever : MonoBehaviour
+{
+    public GameObject audio_switch;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audio_switch.SetActive(true);
+            print("load scene");
+            SceneManager.LoadScene(0);
+
+        }
+    }
+}
